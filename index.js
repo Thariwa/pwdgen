bd456d571a0e5ce0cd5d72aa68159e49c2d56418
Thariwa <umarthariwa@gmail.com>
2023-05-15 10:29:04 -0700

    feat:Password gen

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 let strings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+const exclude = document.getElementById('exclude')
+const include = document.getElementById('include')
+const password = document.getElementById('password')
+const button = document.querySelector('input')
 
 function genpwd(){
+    const strings = [];
+    const include = [Math.floor(Math.random()*strings.length)]
+    const exclude = strings.slice(0, strings.indexOf(0))
+    strings.join('')
     return "password"
-}
+     
+    }
+    
+btn.addEventListener('click', genpwd)
